a1e0fae95be57660a303635e0e897352efcc2d3c
Ayaz <fellasailesi@gmail.com>
2024-07-14 15:12:00 +0300

    Update start.py

--- ArchMusic/plugins/bot/start.py
+++ ArchMusic/plugins/bot/start.py
@@ -116,7 +116,7 @@ async def start_comm(client, message: Message, _):
                 sender_name = message.from_user.first_name
                 return await app.send_message(
                     config.LOG_GROUP_ID,
-                    f"{message.from_user.mention} has just started bot to check <code>SUDOLIST</code>\n\n**USER ID:** {sender_id}\n**USER NAME:** {sender_name}",
+                    f"{message.from_user.mention} Bota Start Verdi. <code>SUDOLIST</code>\n\n**Kullanıcı İd:** {sender_id}\n**Kullanıcı Adı:** {sender_name}",
                 )
             return
         if name[0:3] == "lyr":
