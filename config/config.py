2521a8852c1e5e648135ce654d5eec5fb7dba135
Ayaz <fellasailesi@gmail.com>
2024-07-17 20:02:51 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -18,7 +18,7 @@ API_HASH = getenv("API_HASH", "b88f4594d8b5853add20cdbd4e79b9d0")
 
 ## Get it from @Botfather in Telegram.
 
-BOT_TOKEN = getenv("BOT_TOKEN", "7345857047:AAHBasV2xi66LEQUq75hoWGoODplVOP337M")
+BOT_TOKEN = getenv("BOT_TOKEN", "7345857047:AAHCuHkg1IGsiE2gLL21dOO5kJLdzylE3io")
 
 # Database to save your chats and stats.
 
