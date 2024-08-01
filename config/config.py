64d85ffc25d0b7b5d8cdda4eb5c7784b7ad19d66
BlayzenX <blayzenx@gmail.com>
2024-08-02 02:44:57 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -77,7 +77,7 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
 # GIT TOKEN ( if your edited repo is private)
 
-GIT_TOKEN = getenv("GIT_TOKEN", "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")
+GIT_TOKEN = getenv("GIT_TOKEN", "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")
 
 # Only  Links formats are  accepted for this Var value.
 
