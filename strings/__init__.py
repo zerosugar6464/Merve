8ea8fd2cb009d10a6669006d5b46a9c67d799662
BlayzenX <blayzenx@gmail.com>
2024-08-04 14:16:03 +0300

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,19 +1,24 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2021-2023 by ArchBots@Github, < https://github.com/ArchBots >.
 #
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# This file is part of < https://github.com/ArchBots/ArchMusic > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/ArchBots/ArchMusic/blob/master/LICENSE >
 #
 # All rights reserved.
+#
 
 import os
+import sys
 from typing import List
 
 import yaml
 
 languages = {}
 commands = {}
+
+
+languages_present = {}
 
 
 def get_command(value: str) -> List:
@@ -31,9 +36,29 @@ for filename in os.listdir(r"./strings"):
             open(r"./strings/" + filename, encoding="utf8")
         )
 
+
 for filename in os.listdir(r"./strings/langs/"):
+    if "en" not in languages:
+        languages["en"] = yaml.safe_load(
+            open(r"./strings/langs/en.yml", encoding="utf8")
+        )
+        languages_present["en"] = languages["en"]["name"]
     if filename.endswith(".yml"):
         language_name = filename[:-4]
+        if language_name == "en":
+            continue
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
         )
+        for item in languages["en"]:
+            if item not in languages[language_name]:
+                languages[language_name][item] = languages["en"][item]
+    try:
+        languages_present[language_name] = languages[language_name][
+            "name"
+        ]
+    except:
+        print(
+            "There is some issue with the language file inside bot. Please report it to the ArchBots at @ARCH_SUPPORTS on Telegram"
+        )
+        sys.exit()
