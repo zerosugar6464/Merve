f1c8b92f97420a28774b117883a2b277cad14dc6
BlayzenX <blayzenx@gmail.com>
2024-08-05 13:03:25 +0300

    Update callback.py

--- ArchMusic/plugins/admins/callback.py
+++ ArchMusic/plugins/admins/callback.py
@@ -411,7 +411,7 @@ async def del_back_playlist(client, CallbackQuery, _):
             if (duration_played - duration_to_skip) <= 10:
                 bet = seconds_to_min(duration_played)
                 return await CallbackQuery.answer(
-                    f"Toplam Süre Aşıldığı İçin Bot Arama Yapamıyor‌‌.\n\nŞu anda Oynanan‌‌** {bet}** Dakikadan **{duration}** Dakika",
+                    f"Bot is not able to seek due to total duration has been exceeded.\n\nCurrently played** {bet}** mins out of **{duration}** mins",
                     show_alert=True,
                 )
             to_seek = duration_played - duration_to_skip + 1
@@ -422,7 +422,7 @@ async def del_back_playlist(client, CallbackQuery, _):
             ) <= 10:
                 bet = seconds_to_min(duration_played)
                 return await CallbackQuery.answer(
-                    f"Toplam Süre Aşıldığı için Bot Arama Yapamıyor.\n\nŞuan da Oynanan** {bet}** Dakikadan **{duration}** Dakika",
+                    f"Bot is not able to seek due to total duration has been exceeded.\n\nCurrently played** {bet}** mins out of **{duration}** mins",
                     show_alert=True,
                 )
             to_seek = duration_played + duration_to_skip + 1
@@ -451,4 +451,4 @@ async def del_back_playlist(client, CallbackQuery, _):
         string = _["admin_33"].format(seconds_to_min(to_seek))
         await mystic.edit_text(
             f"{string}\n\nChanges done by: {mention}"
-        )
+      )
