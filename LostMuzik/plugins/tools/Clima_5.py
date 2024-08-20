baa58fc4db0aab4b2b30df417a04452bc8c79b23
BlayzenX <blayzenx@gmail.com>
2024-08-20 20:32:37 +0300

    Update Clima_5.py

--- LostMuzik/plugins/tools/Clima_5.py
+++ LostMuzik/plugins/tools/Clima_5.py
@@ -1,97 +1,130 @@
-import telebot
-import requests
+from aiogram import Bot, Dispatcher, Router, types, F
+from aiogram.filters import CommandStart, Command, CommandObject
+from aiogram.enums import ParseMode
+import python_weather
+
+import asyncio
 import datetime
-from geopy.geocoders import Nominatim
-from config import BOT_TOKEN
+import os
+import math
+
+from settings import *
 
 
-# hava durumu API belirteçlerini tanımlayın 
-WEATHER_TOKEN = 'cbfb1c29dba9b33e67dc1aab3abeae8d'
-POLLING_TIMEOUT = None
-bot = telebot.TeleBot(BOT_TOKEN)
+# Weather
+# p_weather_Bot
 
 
-@bot.message_handler(commands=['weather'])
-def send_weather(message):
-    # This message will be print when /weather command is given, also it will ask user for the location and call fetch_weather
-    location = 'Enter a Location: '
-    sent_message = bot.send_message(message.chat.id, location, parse_mode='Markdown')
-    bot.register_next_step_handler(sent_message, fetch_info)
-    return location
+dp = Dispatcher()
+router = Router(name=__name__)
 
 
-def location_handler(message):
-    # Gets the latitude and longitude from the user's location.
-    location = message.text
-    # Create a geocoder instance
-    geolocator = Nominatim(user_agent="my_app")
-    print(geolocator)
+async def getweather(city: str, is_forecast: bool = True):
+    # declare the client. the measuring unit used defaults to the metric system (celcius, km/h, etc.)
+    async with python_weather.Client(unit=python_weather.METRIC, locale=python_weather.Locale.RUSSIAN) as client:
+        # fetch a weather forecast from a cityn
+        weather = await client.get(city)
 
+        # returns the current day's forecast temperature (int)
+        print(
+            f"Current. t: {weather.current.temperature}, wind: {weather.current.description}, precipitation: {weather.current.precipitation}\n")
+        res = {"current": weather.current}
+
+        # get the weather forecast for a few days
+        print("Forecast.")
+        if not is_forecast:
+            return res
+        res["forecast"] = {}
+        for forecast in weather.forecasts:
+            print(f"{forecast.date} t: {forecast.temperature}")
+            res["forecast"][str(forecast.date)] = {"temperature": forecast.temperature, "unit": forecast.unit,
+                                                   "lowest_temperature": forecast.lowest_temperature,
+                                                   "highest_temperature": forecast.highest_temperature,
+                                                   "astronomy": forecast.astronomy,
+                                                   "snowfall": forecast.snowfall, "sunlight": forecast.sunlight}
+
+            # hourly forecasts
+            for hourly in forecast.hourly:
+                if hourly.time == datetime.time(9, 0):
+                    res["forecast"][str(forecast.date)]["morning"] = hourly
+                    print(
+                        f"  Morning --> t: {hourly.temperature}, precipitation: {hourly.precipitation}, {hourly.description.lower()}")
+                if hourly.time == datetime.time(15, 0):
+                    res["forecast"][str(forecast.date)]["day"] = hourly
+                    print(
+                        f"  Day     --> t: {hourly.temperature}, precipitation: {hourly.precipitation}, {hourly.description.lower()}")
+                if hourly.time == datetime.time(21, 0):
+                    res["forecast"][str(forecast.date)]["evening"] = hourly
+                    print(
+                        f"  Evening --> t: {hourly.temperature}, precipitation: {hourly.precipitation}, {hourly.description.lower()}")
+        return res
+
+
+@dp.message(Command("weather"))
+async def cmd_weather(message: types.Message, command: CommandObject):
+    print("/weather")
+
+    city = command.args
+    if city is None:
+        return await message.reply("Кажется вы забыли указать название города.")
     try:
-        # Get the latitude and longitude
-        location_data = geolocator.geocode(location)
-        print (location_data)
-        latitude = round(location_data.latitude,2)
-        longitude = round(location_data.longitude,2)
-        # print(latitude, longitude)
-        return latitude, longitude
-    except (AttributeError):
-        print("Location not found.")
+        weather = await getweather(city, False)
+    except:
+        return await message.reply("Во время обработки вашего запросы произошла ошибка. Попробуйте ещё раз.")
+    print(weather)
 
+    answer = (f"В городе {city} сейчас {weather['current'].description.lower().replace('дымка', 'туман').replace('снежный ливень', 'дождь со снегом')}, "
+              f"температура воздуха: {weather['current'].temperature}°C, "
+              f"ощущается как {weather['current'].feels_like}°C, влажность: "
+              f"{weather['current'].humidity}%, скорость ветра: "
+              f"{math.floor(weather['current'].wind_speed * 0.27778)} м/с, ")
 
-def get_weather(latitude,longitude):
-    # URL to make API call to OpenWeatherMap API
-    url = 'https://api.openweathermap.org/data/2.5/forecast?lat={}&lon={}&appid={}'.format(latitude, longitude, WEATHER_TOKEN)
-    response = requests.get(url)
-    return response.json()
+    await message.reply(answer)
 
+@dp.message(Command("forecast"))
+async def cmd_forecast(message: types.Message, command: CommandObject):
+    print("/forecast")
 
-# Process weather data, format message, and send to user with unit conversion options
-def fetch_info(message): 
-    '''
-    called when the user provides location in response to the '/weather' command.
-    uses the 'location_handler' function to get latitude & longitude of the provided location and 'get_weather' function to fetch the weather data
-    extracts weather description from API response and sends to user as message.
-    '''
+    city = command.args
+    if city is None:
+        return await message.reply("Кажется вы забыли указать название города.")
     try:
-        latitude, longitude = location_handler(message)
-        
-        weather = get_weather(latitude,longitude)
-        data = weather['list']
-        data_2 = data[0]
+        forecast = await getweather(city, True)
+        forecast = forecast["forecast"]
+    except:
+        return await message.reply("Во время обработки вашего запросы произошла ошибка. Попробуйте ещё раз.")
+    print(forecast)
 
-        info = data_2['weather']
-        data_3 = info[0]
-        main = data_3['main']
+    answer = f"Прогноз погоды на 3 дня для города {city}:\n\n"
+    for date, weather in forecast.items():
+        t = ""
+        if str(datetime.date.today()) == date:
+            t = " (сегодня)"
+        elif str(datetime.date.today() + datetime.timedelta(days=1)) == date:
+            t = " (завтра)"
+        answer += f"""Прогноз погоды на {date}{t}:
+   Утром {weather['morning'].description.lower().replace('дымка', 'туман').replace('снежный ливень', 'дождь со снегом')},
+    температура воздуха: {weather['morning'].temperature}°C,
+    вероятность выпадения осадков: {max(weather['morning'].chances_of_rain, weather['morning'].chances_of_snow)}%
+   Днём {weather['day'].description.lower().replace('дымка', 'туман').replace('снежный ливень', 'дождь со снегом')},
+    температура воздуха: {weather['day'].temperature}°C,
+    вероятность выпадения осадков: {max(weather['day'].chances_of_rain, weather['day'].chances_of_snow)}%
+   Вечером {weather['evening'].description.lower().replace('дымка', 'туман').replace('снежный ливень', 'дождь со снегом')},
+    температура воздуха: {weather['evening'].temperature}°C,
+    вероятность выпадения осадков: {max(weather['evening'].chances_of_rain, weather['evening'].chances_of_snow)}%\n\n"""
+        print(date, weather)
 
-        info = data_2['weather']
-        data_3 = info[0]
-        description = data_3['description'].capitalize()
+    await message.reply(answer)
 
-        dt_txt = data_2['dt_txt']
-        dt = datetime.datetime.strptime(dt_txt, '%Y-%m-%d %H:%M:%S')
-        formatted_date_time = dt.strftime('%A, %B %d, %Y %I:%M %p %Z')
+@dp.message(Command("code"))
+async def cmd_help(message: types.Message):
+    print("/code")
+    await message.reply("Исходны код бота доступен на Github: https://github.com/AlexK-1/Weather-telegram-bot")
 
-        info = data_2['main']
-        temp = info['temp']
+async def main():
+    bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+    await dp.start_polling(bot)
 
-        info = data_2['main']
-        temp_max = info['temp_max']
 
-        info = data_2['main']
-        temp_min = info['temp_min']
-
-        info = data_2['wind']
-        speed = info['speed']
-
-        
-        weather_message = f'*Weather:* {main} - {description}\n\n*Temperature:* {temp:.2f}K\n    *Max Temp:* {temp_max:.2f}K\n    *Min Temp:* {temp_min:.2f}K\n\n*Wind Speed:* {speed}m/s\n\n*Weather info updated on:*\n{formatted_date_time}\n'
-        bot.send_message(message.chat.id, 'Here\'s the weather!')
-        bot.send_message(message.chat.id, weather_message, parse_mode='Markdown')
-
-    except TypeError:
-        bot.send_message(message.chat.id, 'Please enter the valid location')
-            
-print('Starting bot...')
-print('Polling...') 
-bot.infinity_polling()
+if __name__ == "__main__":
+    asyncio.run(main())
