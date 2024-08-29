7584c66c259867c976a73dfc676ed78963478a99
BlayzenX <blayzenx@gmail.com>
2024-08-29 19:19:15 +0300

    Create yenigrup.py

---
+++ LostMuzik/plugins/tools/yenigrup.py
@@ -0,0 +1,24 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from LostMuzik import app
+from LostMuzik.utils.database import get_served_chats
+from config import LOG_GROUP_ID
+
+
+async def lul_message(chat_id: int, message: str):
+    await app.send_message(chat_id=chat_id, text=message)
+
+
+@app.on_message(filters.new_chat_members)
+async def on_new_chat_members(client: Client, message: Message):
+    if (await client.get_me()).id in [user.id for user in message.new_chat_members]:
+        added_by = message.from_user.first_name if message.from_user else "ᴜɴᴋɴᴏᴡɴ ᴜsᴇʀ"
+        matlabi_jhanto = message.chat.title
+        served_chats = len(await get_served_chats())
+        chat_id = message.chat.id
+        if message.chat.username:
+            chatusername = f"@{message.chat.username}"
+        else:
+            chatusername = "🔒 Özel Grup"
+        lemda_text = f"🎉 Bot Yeni Gruba Eklendi ..\n\n┏━━━━━━━━━━━━━━━━━┓\n┣★ **Grup** › : {matlabi_jhanto}\n┣★ **Grup ID** › : {chat_id}\n┣★ **Grup Linki** › : {chatusername}\n┣★ **Toplam Grup** › : {served_chats}\n┣★ **Ekleyen Kişi** › :\n┗━━━ {added_by}"
+        await lul_message(LOG_GROUP_ID, lemda_text)
