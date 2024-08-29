0b6f03fa5ead5e63ad68e623ca70f7d2d4c80cab
BlayzenX <blayzenx@gmail.com>
2024-08-29 16:09:42 +0300

    Update tts.py

--- LostMuzik/plugins/tools/tts.py
+++ LostMuzik/plugins/tools/tts.py
@@ -1,41 +1,11 @@
-import traceback
-from asyncio import get_running_loop
-from io import BytesIO
-
-from googletrans import Translator
+from pyrogram import Client, filters
 from gtts import gTTS
-from pyrogram import Client
-from pyrogram.types import Message
-
-from config import BOT_USERNAME
-from helpers.filters import command
+from LostMuzik import app
 
 
-def convert(text):
-    audio = BytesIO()
-    i = Translator().translate(text, dest="en")
-    lang = i.src
-    tts = gTTS(text, lang=lang)
-    audio.name = lang + ".mp3"
-    tts.write_to_fp(audio)
-    return audio
-
-
-@Client.on_message(command(["tts", f"tts@{BOT_USERNAME}"]))
-async def text_to_speech(_, message: Message):
-    if not message.reply_to_message:
-        return await message.reply_text("🔮 Lütfen bazı mesajlara cevap verin !")
-    if not message.reply_to_message.text:
-        return await message.reply_text("🔮 Lütfen bazı mesajlara cevap verin !")
-    m = await message.reply_text("🔁 Ses Çevriliyor...")
-    text = message.reply_to_message.text
-    try:
-        loop = get_running_loop()
-        audio = await loop.run_in_executor(None, convert, text)
-        await message.reply_audio(audio)
-        await m.delete()
-        audio.close()
-    except Exception as e:
-        await m.edit(str(e))
-        es = traceback.format_exc()
-        print(es)
+@app.on_message(filters.command('tts'))
+def text_to_speech(client, message):
+    text = message.text.split(' ', 1)[1]
+    tts = gTTS(text=text, lang='hi')
+    tts.save('speech.mp3')
+    client.send_audio(message.chat.id, 'speech.mp3')
