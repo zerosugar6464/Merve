3c918481ef0a2723d3807d980bc099d17f13b61f
BlayzenX <blayzenx@gmail.com>
2024-08-30 22:51:18 +0300

    Update bot.py

--- LostMuzik/core/bot.py
+++ LostMuzik/core/bot.py
@@ -8,6 +8,7 @@ import config
 
 from ..logging import LOGGER
 
+
 private_commands = [
     BotCommand("start", "🎧 Botu başlatır"),
     BotCommand("yardim", "📖 Yardım menüsünü gösterir"),
@@ -21,14 +22,16 @@ group_commands = [
     BotCommand("duraklat", "⏸️ Çalan Parçayı Durdurur"),
     BotCommand("devam", "▶️ Çalan Parçayı Devam Ettirir"),
     BotCommand("son", "⏹️ Çalan Parçayı Kapatır"),
+    BotCommand("karistir", "🔀 Çalan Parçayı Karıştırır"),
     BotCommand("dongu", "🔄 Çalan Parçayı Tekrarlar"),
     BotCommand("sira", "📖 Çalma Listelerini Gösterir"),
     BotCommand("ilerisar", "⏩ Parçayı İleri Sarar"),
     BotCommand("gerisar", "⏪ Parçayı Geri Sarar"),
     BotCommand("playlist", "📖 Çalma Listenizi Gösterir"),
     BotCommand("bul", "📩 Seçtiğiniz Parçayı İndirir"),
-    BotCommand("id", "🆔 Kullanıcı ID'sini Verir"),
+    BotCommand("ayarlar", "⚙️ Bot Ayarlarını Gösterir"),
     BotCommand("restart", "🔃 Botu Yeniden Başlatır"),
+    BotCommand("id", "🆔 Kullanıcı ID'sini Verir"),
     BotCommand("hava", "🌦️ Hava Durumunu Gösterir"),
     BotCommand("reload", "❤️‍🔥 Yönetici Önbelleğini Günceller"),
     
@@ -41,42 +44,55 @@ async def set_commands(client):
     
     await client.set_bot_commands(group_commands, scope=BotCommandScopeAllGroupChats())
 
-class LostMuzik(Client):
+class ArchMusic(Client):
     def __init__(self):
+        LOGGER(__name__).info(f"Bot Başlatılıyor")
         super().__init__(
             "LostMuzik",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
-            in_memory=True,
         )
-        LOGGER(__name__).info(f"Bot Başlatılıyor...")
 
     async def start(self):
         await super().start()
-        get_me = await self.get_me()
-        self.username = get_me.username
-        self.id = get_me.id
-        self.mention = get_me.mention
         try:
-            await self.send_message(
-                config.LOG_GROUP_ID, "» **Bot Başarılı Bir Şekilde Başlatıldı** 🔮"
-                
-            )
-        except:
-            LOGGER(__name__).error(
-                "Bot log grubuna erişemedi. Log kanalınıza botunuzu eklediğinizden ve yönetici olarak terfi ettirdiğinizden emin olun!"
-            )
-            sys.exit()
+            get_me = await self.get_me()
+            self.username = get_me.username
+            self.id = get_me.id
+
+            video_url = "https://telegra.ph/file/36221d40afde82941ffff.mp4"
+            caption = "__Bot Başlatılıyor . . . ⚡️__"
             
-            await set_commands(self)
-            
-        a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
-        if a.status != ChatMemberStatus.ADMINISTRATOR:
-            LOGGER(__name__).error("Lütfen Logger Grubunda Botu Yönetici Olarak Terfi Ettirin.")
+            try:
+                await self.send_video(
+                    config.LOG_GROUP_ID,
+                    video=video_url,
+                    caption=caption,
+                )
+            except:
+                LOGGER(__name__).error(
+                    "Bot log grubuna erişemedi. Log kanalınıza botunuzu eklediğinizden ve yönetici olarak terfi ettirdiğinizden emin olun!"
+                )
+                sys.exit()
+
+            await set_commands(self)  
+
+
+            a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
+            if a.status != ChatMemberStatus.ADMINISTRATOR:
+                LOGGER(__name__).error(
+                    "Lütfen Logger Grubunda Botu Yönetici Olarak Terfi Ettirin"
+                )
+                sys.exit()
+
+        except Exception as e:
+            LOGGER(__name__).error(f"Bot başlatılırken hata oluştu: {e}")
             sys.exit()
+
         if get_me.last_name:
             self.name = get_me.first_name + " " + get_me.last_name
         else:
             self.name = get_me.first_name
-        LOGGER(__name__).info(f" {self.name} olarak başlatıldı")
+
+        LOGGER(__name__).info(f"{self.name} olarak başlatıldı")
