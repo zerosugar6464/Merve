26679477a0abf5de16021b4b3e6c90fc1c0a71da
BlayzenX <blayzenx@gmail.com>
2024-08-30 23:18:29 +0300

    Update bot.py

--- LostMuzik/core/bot.py
+++ LostMuzik/core/bot.py
@@ -44,11 +44,11 @@ async def set_commands(client):
     
     await client.set_bot_commands(group_commands, scope=BotCommandScopeAllGroupChats())
 
-class InsaneBot(Client):
+class LostMuzik(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Bot Başlatılıyor...")
         super().__init__(
-            "MusicBot",
+            "LostMuzik",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
