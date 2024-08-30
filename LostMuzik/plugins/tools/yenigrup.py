53f493ffa756fe28d16edd7f34ef5aac2a46cb95
BlayzenX <blayzenx@gmail.com>
2024-08-30 20:54:59 +0300

    Update yenigrup.py

--- LostMuzik/plugins/tools/yenigrup.py
+++ LostMuzik/plugins/tools/yenigrup.py
@@ -1,24 +1,52 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from config import LOGGER_ID
 from LostMuzik import app
-from LostMuzik.utils.database import get_served_chats
-from config import LOG_GROUP_ID
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-async def lul_message(chat_id: int, message: str):
-    await app.send_message(chat_id=chat_id, text=message)
+async def new_message(chat_id: int, message: str, reply_markup=None):
+    await app.send_message(chat_id=chat_id, text=message, reply_markup=reply_markup)
 
 
 @app.on_message(filters.new_chat_members)
 async def on_new_chat_members(client: Client, message: Message):
     if (await client.get_me()).id in [user.id for user in message.new_chat_members]:
-        added_by = message.from_user.first_name if message.from_user else "Gizli Kullanıcı"
-        matlabi_jhanto = message.chat.title
-        served_chats = len(await get_served_chats())
+        added_by = f"{message.from_user.first_name}"
+        chatusername = f"@{message.chat.username}"
+        title = message.chat.title
         chat_id = message.chat.id
-        if message.chat.username:
-            chatusername = f"@{message.chat.username}"
-        else:
-            chatusername = "Gizli Grup 🔒"
-        lemda_text = f"#Yeni Gruba Eklendi..\n\n**Grup Adı**: {matlabi_jhanto}\n**Grup ID**: {chat_id}\n**Grup Linki**: {chatusername}\n**Toplam Grup**: {served_chats}\n**Ekleyen Kişi**: {added_by}"
-        await lul_message(LOG_GROUP_ID, lemda_text)
+        sigma = f" <u>#**Yeni Gruba Eklendi**</u> :\n\n**Grup ID:** {chat_id}\n**Grup Adı:** {title}\n**Grup Link:** {chatusername}\n**Gruba Ekleyen:** {added_by}"
+
+        reply_markup = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, user_id=message.from_user.id
+                    )
+                ]
+            ]
+        )
+
+        await new_message(LOGGER_ID, sigma, reply_markup)
+
+
+@app.on_message(filters.left_chat_member)
+async def on_left_chat_member(client: Client, message: Message):
+    if (await client.get_me()).id == message.left_chat_member.id:
+        removed_by = f"{message.from_user.first_name}"
+        title = message.chat.title
+        chat_id = message.chat.id
+        bye = f" <u>#**Gruptan Çıkarıldı**</u> :\n\n**Grup ID:** {chat_id}\n**Grup Adı:** {title}\n**Gruptan Çıkaran:** {removed_by}"
+
+        reply_markup = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, user_id=message.from_user.id
+                    )
+                ]
+            ]
+        )
+
+        await new_message(LOGGER_ID, bye, reply_markup)
