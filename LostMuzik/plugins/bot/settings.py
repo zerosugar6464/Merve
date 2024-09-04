6ec3bc06a71095e3762ed615131b1627435f3175
BlayzenX <blayzenx@gmail.com>
2024-09-04 16:50:02 +0300

    Update settings.py

--- LostMuzik/plugins/bot/settings.py
+++ LostMuzik/plugins/bot/settings.py
@@ -1,13 +1,3 @@
-#
-# Copyright (C) 2021-2023 by LostBots@Github, < https://github.com/LostBots >.
-#
-# This file is part of < https://github.com/LostBots/LostMuzik > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/LostBots/LostMuzik/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.errors import MessageNotModified
